bc7a8b5ad334333b570b5c843b0bfc83c58a2b6e
Chetan Jangir <chetanrjj2002@gmail.com>
2026-01-12 12:54:35 +0530

    feat(utils): Add encrypt function

--- internal/tui/utils/utils.go
+++ internal/tui/utils/utils.go
@@ -6,6 +6,7 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/chetanjangir0/onepdfplease/internal/tui/messages"
 	"github.com/pdfcpu/pdfcpu/pkg/api"
+	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu/model"
 )
 
 func Merge(inFiles []string, outFile string) tea.Cmd {
@@ -29,4 +30,33 @@ func Merge(inFiles []string, outFile string) tea.Cmd {
 			TaskType: tasktype,
 		}
 	}
+}
+
+func Encrypt(inFile, outFile string, password string) tea.Cmd {
+	return func() tea.Msg {
+		taskType := "Encrypt"
+		if len(password) == 0 {
+			return messages.PDFOperationStatus{
+				TaskType: taskType,
+				Err:      fmt.Errorf("Please provide a password"),
+			}
+
+		}
+		conf := model.NewDefaultConfiguration()
+		conf.UserPW = password
+		conf.OwnerPW = password
+
+		err := api.EncryptFile(inFile, outFile, conf)
+		if err != nil {
+			return messages.PDFOperationStatus{
+				TaskType: taskType,
+				Err:      err,
+			}
+		}
+		return messages.PDFOperationStatus{
+			TaskType: taskType,
+		}
+
+	}
+
 }
